610c5b122ad32f98e290c3fd6a45c3c2fd22c571
Sajib Debnath <debnathsajib2024@gmail.com>
2021-12-01 00:00:00 -0800

    Updates

---
+++ precache-manifest.433a5357e2181e0075fb8bd037b6a31f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ae4a8a15da72b509bc8f6659c51e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c72c539d90342f00ff6e",
+    "url": "/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "ded50524eda97edb5949",
+    "url": "/static/css/main.afcfd3df.chunk.css"
+  },
+  {
+    "revision": "c72c539d90342f00ff6e",
+    "url": "/static/js/2.22b73357.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.22b73357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded50524eda97edb5949",
+    "url": "/static/js/main.67c3ed41.chunk.js"
+  },
+  {
+    "revision": "60949e83de0aeb529ab1",
+    "url": "/static/js/runtime-main.25c7e1d9.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/static/media/software.43049cae.jpeg"
+  }
+]);
